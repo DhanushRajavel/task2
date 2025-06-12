e05a5f538ac08421713f8d3b324f4abe56ecd811
DhanushRajavel <dhanushrajavel24@gmail.com>
2025-06-12 13:00:01 +0530

    firebase disti added

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,23 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:task2/model/user.dart';
 import 'providers/user_provider.dart';
 import 'views/user_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(UserAdapter());
 
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => UserProvider(),
-      child: const MyApp(),
-    ),
+    ChangeNotifierProvider(create: (_) => UserProvider(), child: const MyApp()),
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp();
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
